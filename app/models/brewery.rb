5c1359d890a256634abab1d34e62d3c327856130
Brian Burt <beeburrt@outlook.com>
2023-06-15 22:58:12 -0700

    Left off on exercise 15

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,9 +1,16 @@
 class Brewery < ApplicationRecord
-  has_many :beers
+  has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def print_report
     puts name
     puts "established at year #{year}"
     puts "number of beers #{beers.count}"
+  end
+
+  def average_rating
+    sum = ratings.map { |r| r[:score] }.reduce(0) { |s, i| s + i }
+    count = ratings.count
+    (sum / count).to_f
   end
 end
