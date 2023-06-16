162fa0e79f3cb157e26af96206b7aa8f929073d5
Brian Burt <beeburrt@outlook.com>
2023-06-15 21:33:55 -0700

    Finish exercise 9

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -12,6 +12,7 @@ class BeersController < ApplicationController
   # GET /beers/new
   def new
     @beer = Beer.new
+    @breweries = Brewery.all
   end
 
   # GET /beers/1/edit
@@ -23,8 +24,8 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to beer_url(@beer), notice: 'Beer was successfully created.' }
-        format.json { render :show, status: :created, location: @beer }
+        format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
+        format.json { render :index, status: :created, location: beers_path }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @beer.errors, status: :unprocessable_entity }
