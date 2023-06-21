31738c884e19638eaeb0a8619c85d4b1cfe1c96c
Brian Burt <beeburrt@outlook.com>
2023-06-21 16:00:29 -0700

    Add .rubocop.yml

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,7 +2,7 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
-    sum = ratings.map { |r| r[:score] }.reduce(0) { |s, i| s + i }
+    sum = ratings.map { |r| r.score }.sum
     count = ratings.count
     (sum / count).to_f
   end
